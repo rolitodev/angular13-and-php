6cd54cb580e39d15ecf1038362f589d0f5955b9f
rolitodev <hectorj.24@hotmail.com>
2022-06-08 16:46:58 -0500

    Fix env data

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: "https://backendbuilo.alwaysdata.net/server",
+  baseUrl: "https://inmobiliariadccn.alwaysdata.net/server",
 };
 
 /*
